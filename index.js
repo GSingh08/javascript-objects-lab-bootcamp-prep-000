3a156245349ae042fa60ccf306f02fb5fffa8202
Gurjot Singh <gurjotssidhu08@gmail.com>
2018-01-11 23:45:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ obj[key] = value;
 return obj;
 }
 
-function deleteFromObjectByKey(obj, key){
+function deleteFromObjectByKey(object, key) {
+  const newObj = Object.assign({}, object)
 
- 
+  delete newObj[key]
+
+  return newObj
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key]
+
+  return object
 }
